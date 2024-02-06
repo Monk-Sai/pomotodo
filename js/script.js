133b599c24cf0d4a79adb08056332e2dab287420
Super Sai-yan Monk <acharyasaiprasad@gmail.com>
2024-02-07 02:22:42 +0530

    js fixes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 // variables
 let workTitle = document.getElementById('work');
 let breakTitle = document.getElementById('break');
+let breakTitleTwo = document.getElementById('break2');
+
 
 let workTime = 25;
-let breakTime = 1;
+let breakTime = 5;
+let breakTime2 = 10;
+
 
 let seconds = "00"
 let pomodoroCount = 0;
@@ -52,23 +56,36 @@ function start() {
                     breakTitle.classList.add('active');
                     breakTitleTwo.classList.remove('active');
                 }else if (pomodoroCount === 1) {
+                    // start a 25-minute work after the first Break
+                    workMinutes = 25; 
+                    pomodoroCount++;
+
+                    // change panel
+                    workTitle.classList.add('active');
+                    breakTitle.classList.remove('active');
+                    breakTitleTwo.classList.remove('active');
+                }else if (pomodoroCount === 2) {
                     // start a 10-minute break after the second Pomodoro
-                    workMinutes = 9; // 10 minutes minus 1
+                    workMinutes = 10; 
                     pomodoroCount++;
 
                     // change panel
                     workTitle.classList.remove('active');
-                    breakTitle.classList.add('active');
-                }else {
-                    workMinutes = workTime;
-                    pomodoroCount = 0;
+                    breakTitle.classList.remove('active');
+                    breakTitleTwo.classList.add('active');
+                }
+                else if (pomodoroCount === 3) {
+
+                    workMinutes = 25;
+                    clearInterval(timerInterval); // Stop the timer using the stored ID
 
                     // change panel
-                    breakTitle.classList.add('active');
-                    workTitle.classList.remove('active');
+                    breakTitle.classList.remove('active');
+                    workTitle.classList.add('active');
+                    breakTitleTwo.classList.remove('active');
                 }
             }
-        seconds = 59;
+            seconds = 59;
         }
     }
     // start Countdown
